fa7a1e7bcf3d6862e194570e64431884680b1a98
oscar urgelles <oscar.urgelles@gmail.com>
2019-03-21 18:36:40 +0100

    validating script

--- modulo2/scripts/main.js
+++ modulo2/scripts/main.js
@@ -1,7 +1,8 @@
 // function validacion(form) {
 
-//   valor = document.getElementById("first_name").value;
 
+// //Validamos First Name
+// valor = document.getElementById("first_name").value;
 //   var re = /^[\w ]+$/;
 //   if (valor == null || valor.length < 2 || /^\s+$/.test(valor) || !re.test(valor)) {
 //     alert('[ERROR] First Name contains invalid characters!');
@@ -13,204 +14,289 @@
 //   return true;
 // }
 
-var forms = document.querySelectorAll('.validate');
-for (var i = 0; i < forms.length; i++) {
-  forms[i].setAttribute('novalidate', true);
-}
 
 
-// Validate the field
+
+
 var hasError = function (field) {
 
-  // Don't validate submits, buttons, file and reset inputs, and disabled fields
-  if (field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') return;
+    // Don't validate submits, buttons, file and reset inputs, and disabled fields
+    if (field.disabled || field.type === 'file' || field.type === 'reset' || field.type === 'submit' || field.type === 'button') return;
 
-  // Get validity
-  var validity = field.validity;
+    // Get 
+    
+  
+    if (field.id == "first_name") {
+        var valor=field.value;
+        var re = /[^a-zA-Z]/;
+        var joined = valor.split(" ").join("");
+        if (valor == null || joined.length < 2) {
+            return 'Please fill out this field 2 or more characters.';
+        }
+        else if (/^[\s]+/.test(valor)) {
+            return 'Can not have spaces at the beginning';
+        }
+        else if (re.test(joined)) {
+            return '4!.?2(@9# etc.. incorrect characters or numbers';
+        }
+        else return;
+    }
+    else if (field.id == "last_name") {
+        var valor=field.value;
+        var re = /[^a-zA-Z]/;
+        var joined = valor.split(" ").join("");
+        if (valor == null || joined.length < 2) {
+            return 'Please fill out this field 2 or more characters.';
+        }
+        else if (/^[\s]+/.test(valor)) {
+            return 'Can not have spaces at the beginning';
+        }
+        else if (re.test(joined)) {
+            return '!.?¿)(@# etc.. incorrect characters or numbers';
+        }
+        else return;
+    }
 
-  // If valid, return null
-  if (validity.valid) return;
+    else if (field.id == "adr") {
+        var valor=field.value;
+        var joined = valor.split(" ").join("");
+        if (valor == null || joined.length < 2) {
+            return 'Please fill out this field 2 or more characters.';
+        }
+        else if (/^[\s]+/.test(valor)) {
+            return 'Can not have spaces at the beginning';
+        }  
+        else return;
+    }
 
-  // If field is required and empty
-  if (validity.valueMissing) return 'Please fill out this field.';
+    else if (field.id == "city") {
+        var valor=field.value;
+        var re = /[^a-zA-Z]/;
+        var joined = valor.split(" ").join("");
+        if (valor == null || joined.length < 2) {
+            return 'Please fill out this field 2 or more characters.';
+        }
+        else if (/^[\s]+/.test(valor)) {
+            return 'Can not have spaces at the beginning';
+        }
+        else if (re.test(joined)) {
+            return '!.?¿)(@# etc.. incorrect characters or numbers';
+        }
+        else return;
+    }
 
-  // If not the right type
-  if (validity.typeMismatch) {
-    // Email
-    if (field.type === 'email') return 'Please enter an email address.';
-  }
+    else if (field.id == "zip") {
+        var valor=field.value;
+        var re = /^[0-9]{5}$/;
+        var joined = valor.split(" ").join("");
+        if (valor == null || joined.length < 2) {
+            return 'Please fill out this field 2 or more characters.';
+        }
+        else if (/^[\s]+/.test(valor)) {
+            return 'Can not have spaces at the beginning';
+        }
+        else if (!re.test(joined)) {
+            return 'Zip Code has 5 numbers';
+        }
+        else return;
+    }
 
-  // If too short
-  if (validity.tooShort) return 'Please lengthen this text to ' + field.getAttribute('minLength') + ' characters or more. You are currently using ' + field.value.length + ' characters.';
+    else if (field.id == "parent") {
+        var valor=field.value;
+        var re = /[^a-zA-Z]/;
+        var joined = valor.split(" ").join("");
+        if (valor == null || joined.length < 2) {
+            return 'Please fill out this field 2 or more characters.';
+        }
+        else if (/^[\s]+/.test(valor)) {
+            return 'Can not have spaces at the beginning';
+        }
+        else if (re.test(joined)) {
+            return '4!.?2(@9# etc.. incorrect characters or numbers';
+        }
+        else return;
+    }
 
-  // If too long
-  if (validity.tooLong) return 'Please shorten this text to no more than ' + field.getAttribute('maxLength') + ' characters. You are currently using ' + field.value.length + ' characters.';
+    
+    else if (field.id == "phone") {
+        var valor=field.value;
+        var re = /^\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})$/;
+        var joined = valor.split(" ").join("");
+        if (valor == null || joined.length < 2) {
+            return 'Please fill this field whit a real phone number.';
+        }
+        else if (/^[\s]+/.test(valor)) {
+            return 'Can not have spaces at the beginning';
+        }
+        else if (!re.test(joined)) {
+            return 'Tipical PhoneNumber : "(123) 456 7899"';
+        }
+        else return;
+    }
 
-  // If number input isn't a number
-  if (validity.badInput) return 'Please enter a number.';
+    else if (field.id == "mail") {
+        var valor=field.value;
+        var re = /\S+@\S+\.\S+/;
+        var joined = valor.split(" ").join("");
+        var space = /^[\s]+/;
+        console.log(valor);
+        if (valor == null || joined.length < 2) {
+            return 'Please fill this field whit real Email';
+        }
+        else if (space.test(valor)) {
+            
+            return 'Can not have spaces at the beginning';
+        }
+        else if (!re.test(joined)) {
+            return 'Tipical email yourname@gmail.com';
+        }
+        else return;
+    }
+    
 
-  // If a number value doesn't match the step interval
-  if (validity.stepMismatch) return 'Please select a valid value.';
+    // form.first_name.focus();
+    // form.first_name.style.borderColor = "red";
 
-  // If a number field is over the max
-  if (validity.rangeOverflow) return 'Please select a value that is no more than ' + field.getAttribute('max') + '.';
+    // return false;
 
-  // If a number field is below the min
-  if (validity.rangeUnderflow) return 'Please select a value that is no less than ' + field.getAttribute('min') + '.';
 
-  // If all else fails, return a generic catchall error
-  return 'The value you entered for this field is invalid.';
+    // If all else fails, return a generic catchall error
+    // return 'The value you entered for this field is invalid.';
 
 };
+
 
 // Show an error message
 var showError = function (field, error) {
 
-  // Add error class to field
-  field.classList.add('error');
-
-  // If the field is a radio button and part of a group, error all and get the last item in the group
-  if (field.type === 'radio' && field.name) {
-      var group = document.getElementsByName(field.name);
-      if (group.length > 0) {
-          for (var i = 0; i < group.length; i++) {
-              // Only check fields in current form
-              if (group[i].form !== field.form) continue;
-              group[i].classList.add('error');
-          }
-          field = group[group.length - 1];
-      }
-  }
-
-  // Get field id or name
-  var id = field.id || field.name;
-  if (!id) return;
-
-  // Check if error message field already exists
-  // If not, create one
-  var message = field.form.querySelector('.error-message#error-for-' + id );
-  if (!message) {
-      document.getElementById(id).style.width = "200px";
-      message = document.createElement('div');
-      message.className = 'error-message maxsize txtcenter';
-      message.id = 'error-for-' + id;
-      
-      // If the field is a radio button or checkbox, insert error after the label
-      var label;
-      if (field.type === 'radio' || field.type ==='checkbox') {
-          label = field.form.querySelector('label[for="' + id + '"]') || field.parentNode;
-          if (label) {
-              label.parentNode.insertBefore( message, label.nextSibling );
-          }
-      }
-
-      // Otherwise, insert it after the field
-      if (!label) {
-          field.parentNode.insertBefore( message, field.nextSibling );
-      }
-
-  }
+    var valor =field.value;
+    // Add error class to field
+    field.classList.add('error');
+    field.classList.add('error-message');
+    field.classList.add('Red');
+    if(valor.length <2){
+        field.placeholder=error;
+        field.value=null;
+    }
+    else {
+        field.placeholder = "("+valor +") "+ error;
+        field.value = null;
+    }
+ 
   
-  // Add ARIA role to the field
-  field.setAttribute('aria-describedby', 'error-for-' + id);
-
-  // Update error message
-  message.innerHTML = error;
-
-  // Show error message
-  message.style.display = 'block';
-  message.style.visibility = 'visible';
-
-};
-
-// Remove the error message
-var removeError = function (field) {
-
-  // Remove error class to field
-  field.classList.remove('error');
   
-  // Remove ARIA role from the field
-  field.removeAttribute('aria-describedby');
-
-  // If the field is a radio button and part of a group, remove error from all and get the last item in the group
-  if (field.type === 'radio' && field.name) {
-      var group = document.getElementsByName(field.name);
-      if (group.length > 0) {
-          for (var i = 0; i < group.length; i++) {
-              // Only check fields in current form
-              if (group[i].form !== field.form) continue;
-              group[i].classList.remove('error');
-          }
-          field = group[group.length - 1];
-      }
-  }
-
-  // Get field id or name
-  var id = field.id || field.name;
-  if (!id) return;
+    // // If the field is a radio button and part of a group, error all and get the last item in the group
+    // if (field.type === 'radio' && field.name) {
+    //     var group = document.getElementsByName(field.name);
+    //     if (group.length > 0) {
+    //         for (var i = 0; i < group.length; i++) {
+    //             // Only check fields in current form
+    //             if (group[i].form !== field.form) continue;
+    //             group[i].classList.add('error');
+    //         }
+    //         field = group[group.length - 1];
+    //     }
+    // }
   
+    // Get field id or name
+    // var id = field.id;
+    // if (!id) return;
+  
+    // Check if error message field already exists
+    // If not, create one
+    
+    
+  };
 
-  // Check if an error message is in the DOM
-  var message = field.form.querySelector('.error-message#error-for-' + id + '');
-  if (!message) return;
 
-  // If so, hide it
-  message.innerHTML = '';
-  message.style.display = 'none';
-  message.style.visibility = 'hidden';
+  var removeError = function (field) {
 
-};
+    // Remove error class to field
+    field.classList.remove('error');
+    field.classList.remove('error-message');
+    field.classList.remove('Red');
+    
+    // // If the field is a radio button and part of a group, remove error from all and get the last item in the group
+    // if (field.type === 'radio' && field.name) {
+    //     var group = document.getElementsByName(field.name);
+    //     if (group.length > 0) {
+    //         for (var i = 0; i < group.length; i++) {
+    //             // Only check fields in current form
+    //             if (group[i].form !== field.form) continue;
+    //             group[i].classList.remove('error');
+    //         }
+    //         field = group[group.length - 1];
+    //     }
+    // }
+  
+    // // Get field id or name
+    // var id = field.id || field.name;
+    // if (!id) return;
+    
+  
+    // // Check if an error message is in the DOM
+    // var message = field.form.querySelector('.error-message#error-for-' + id + '');
+    // if (!message) return;
+  
+    // // If so, hide it
+    // message.innerHTML = '';
+    // message.style.display = 'none';
+    // message.style.visibility = 'hidden';
+  
+  };
+
 
 // Listen to all blur events
 document.addEventListener('blur', function (event) {
 
-  // Only run if the field is in a form to be validated
-  if (!event.target.form.classList.contains('validate')) return;
+    // Only run if the field is in a form to be validated
+    if (!event.target.form.classList.contains('validate')) return;
 
-  // Validate the field
-  var error = hasError(event.target);
-
-  // If there's an error, show it
-  if (error) {
-      showError(event.target, error);
-      return;
-  }
-
-  // Otherwise, remove any existing error message
-  removeError(event.target);
+    // Validate the field
+    var error = hasError(event.target);
+    // If there's an error, show it
+    if (error) {
+        showError(event.target, error);
+        return;
+    }
+    // Otherwise, remove any existing error message
+    removeError(event.target);
 
 }, true);
 
 
-// Check all fields on submit
-document.addEventListener('submit', function (event) {
 
-  // Only run on forms flagged for validation
-  if (!event.target.classList.contains('validate')) return;
 
-  // Get all of the form elements
-  var fields = event.target.elements;
 
-  // Validate each field
-  // Store the first field with an error to a variable so we can bring it into focus later
-  var error, hasErrors;
-  for (var i = 0; i < fields.length; i++) {
-      error = hasError(fields[i]);
-      if (error) {
-          showError(fields[i], error);
-          if (!hasErrors) {
-              hasErrors = fields[i];
-          }
-      }
-  }
+//   // Check all fields on submit
+//   document.addEventListener('submit', function (event) {
 
-  // If there are errrors, don't submit form and focus on first element with error
-  if (hasErrors) {
-      event.preventDefault();
-      hasErrors.focus();
-  }
+//     // Only run on forms flagged for validation
+//     if (!event.target.classList.contains('validate')) return;
 
-  // Otherwise, let the form submit normally
-  // You could also bolt in an Ajax form submit process here
+//     // Get all of the form elements
+//     var fields = event.target.elements;
 
-}, false);
+//     // Validate each field
+//     // Store the first field with an error to a variable so we can bring it into focus later
+//     var error, hasErrors;
+//     for (var i = 0; i < fields.length; i++) {
+//         error = hasError(fields[i]);
+//         if (error) {
+//             showError(fields[i], error);
+//             if (!hasErrors) {
+//                 hasErrors = fields[i];
+//             }
+//         }
+//     }
+
+//     // If there are errrors, don't submit form and focus on first element with error
+//     if (hasErrors) {
+//         event.preventDefault();
+//         hasErrors.focus();
+//     }
+
+//     // Otherwise, let the form submit normally
+//     // You could also bolt in an Ajax form submit process here
+
+//   }, false);
