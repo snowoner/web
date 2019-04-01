269e35a02ec956b2da73cc0d9947ee2b21c210eb
oscar urgelles <oscar.urgelles@gmail.com>
2019-04-02 00:51:02 +0200

    task3 previous

---
+++ modulojs/task3/house-party-loyalty.js
@@ -0,0 +1,6 @@
+var statics =
+{
+"numberOfDemocrats":"0",
+"numberOfRepublicans":"0",
+"numberOfIndependents":"0"
+};
---
+++ modulojs/task3/senate-attendance.js
@@ -0,0 +1,11 @@
+var statics =
+{
+"numberOfDemocrats":"0",
+"numberOfRepublicans":"0",
+"numberOfIndependents":"0"
+};
+
+// Republicans and Independents
+// how Democrats and Republicans compare, on average, for voting with their party
+// which members most often do not vote with their party, which ones most often do vote with their party
+// which members have missed the most votes, which have missed the least
